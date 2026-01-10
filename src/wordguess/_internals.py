d1ff87170f562fa0d5f664003926981454c3591f
Sarisha Das <sarishadas.810@gmail.com>
2026-01-09 17:22:49 -0800

    _internals added to temporarily store sample corpus

---
+++ src/wordguess/_internals.py
@@ -0,0 +1,12 @@
+minidict = [
+    "apple", "baker", "chair", "delta", "eagle", "frame", "grape", "house", "ivory", "joker",
+    "knife", "lemon", "mango", "night", "ocean", "piano", "queen", "river", "stone", "tiger",
+    "unity", "value", "wheat", "mount", "young", "zebra", "alert", "brave", "crane", "dream",
+    "enemy", "field", "giant", "heart", "input", "judge", "about", "layer", "metal", "north",
+    "olive", "pride", "quest", "rough", "sharp", "table", "under", "visit", "world", "yield",
+    "aside", "blush", "cloud", "dwarf", "empty", "fresh", "glide", "honor", "inner", "light",
+    "march", "noble", "other", "plain", "quiet", "ratio", "smart", "trust", "upper", "shout",
+    "woman", "years", "angle", "beach", "crown", "drive", "exact", "flame", "green", "habit",
+    "index", "logic", "money", "nurse", "offer", "patch", "quick", "royal", "scale", "thick",
+    "urban", "video", "worth", "extra", "basic", "clean", "focus", "happy", "ideal", "smile"
+]
