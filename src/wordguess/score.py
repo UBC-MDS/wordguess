be6e7bc057432ab808744d7c5cb7d6da254e3ca0
Harry Yau <yauchungki1305@gmail.com>
2026-01-10 10:58:49 -0800

    edit explanation and change parameter names

--- src/wordguess/score.py
+++ src/wordguess/score.py
@@ -1,29 +1,42 @@
 # score.py
 # author: Harry Yau
 # date 2026-01-09
-"""
-A module that calculate the score of the guessing word.
-this function will take a list of integer list as input and return a score
 
-"""
-def get_score(guesses: list[list[int]], penalty: bool = False) -> float:
+def get_score(result: list[str], penalty: bool = False, penalty_rate: float = 0.0) -> float:
     """
     Calculate the score of the guessing.
 
-    This function computes the score based on the provided guesses.
+    This function computes the score based on the provided guess result.
+    The calculation will be the highest score from the result list.
+    each guess will contain characters '0', '1', and '2' representing different guess outcomes.
+    
+    calculation will be sum of of the highest guess score diveded by the total score of the words.
+    for example, if the highest guess score is "12222"
+    the returning score will be (1+2+2+2+2)/10 * 100 = 90.0
+
+    By default, no penalty is applied. If penalty is set to True, a penalty will be applied for each guess
+    before the correct guess. The penalty is calculated as follows:
+    For each guess before the correct guess, the score is reduced by the penalty rate of the total score.
+    For example, if the correct guess is on the 3rd attempt and the penalty_rate is 0.1, the score will be reduced by 20%.
 
     Parameters
     ----------
-    guesses : list of list of int
-        A list containing lists of integers representing guesses.
+    result : list of str
+        A list containing the list of result. The result is a string
+        with characters '0', '1', and '2' representing different guess outcomes.
 
     penalty : bool, optional
         A flag indicating whether to apply a penalty, by default False.
+        penalty is applied for each guess before the correct guess.
+
+    penalty_rate : float, optional
+        The rate at which the penalty is applied for each incorrect guess, by default 0.0.
+        For example, a penalty_rate of 0.1 means a 10% reduction in score for each incorrect guess.
 
     Returns
     -------
     float
-        The calculated score based on the guesses and penalty flag.
+        The calculated score based on the result and penalty flag.
 
     Examples
     --------
@@ -31,7 +44,7 @@ def get_score(guesses: list[list[int]], penalty: bool = False) -> float:
     100.0
     >>> get_score([[0, 0, 0, 0, 0], [2, 2, 2, 2, 2]])
     100.0
-    >>> get_score([[0, 1, 1, 2, 2], [0, 2, 2, 2, 2], [2, 2, 2, 2, 2]], penalty=True)
+    >>> get_score([[0, 1, 1, 2, 2], [0, 2, 2, 2, 2], [2, 2, 2, 2, 2]], penalty=True, 0.1)
     81.0
     >>> get_score([[0, 0, 0, 0, 0], [2, 1, 2, 2, 2]])
     0.0
