4240065d1d11b68f829ff43e0b06cd12156cfdaf
Yuheng Ouyang <yuhengo@student.ubc.ca>
2026-02-02 02:35:43 -0800

    Include words to run the examples

--- src/wordguess/_internals.py
+++ src/wordguess/_internals.py
@@ -566,6 +566,7 @@ minidict = [
     "harry",
     "sweep",
     "manor",
+    "slope",
     "guild",
     "foray",
     "altar",
@@ -635,5 +636,7 @@ minidict = [
     "exult",
     "candy",
     "might",
-    "spark"
+    "spark",
+    "apply",
+    "stare"
 ]
